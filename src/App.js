4af70685dc323fcf45a65569a0f346b2a00ee3c9
melahdal <elahdalmohammed@gmail.com>
2025-04-29 10:43:02 +0200

    commit revolte

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-
 import GoogleDocsViewer from 'react-google-docs-viewer';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Example of react-google-docs-viewer</h1>
-        </header>
-        
-        <GoogleDocsViewer
-          width="600px"
-          height="780px"
-          fileUrl="https://github.com/mozilla/pdf.js/raw/master/examples/helloworld/helloworld.pdf"
-        />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>Revolte</h1>
+      <h2>Visualiser vos documents en toute sécurité 👇</h2>
+
+      <GoogleDocsViewer
+        width="600px"
+        height="780px"
+        fileUrl="https://docs.google.com/document/d/1una3HmF4gGPgf1mP7VzKMI0QD-2ACMefsTNk9fv5JzU/export?format=pdf"
+        // on affiche le document a partir d'une url accesible (voir piste usage unique)
+        disableDownload={true} 
+        // ici j'ai enlevé le bouton télecharger mais faut aussi l'enlever des droits 
+      />
+    </div>
+  );
 }
 
 export default App;
+
+
+// Deuxime modele avec Iframe 
+
+// import React from 'react';
+// import './App.css';
+// import GoogleDocsViewer from 'react-google-docs-viewer';
+
+// function App() {
+//   return (
+//     <div className="App">
+//        <h1>Revolte</h1>
+//        <h2>Visualiser vos documents en toute sécurité 👇</h2>
+
+//        <iframe
+//          title="Google Doc"
+//          src="https://docs.google.com/document/d/1UfwlD68u6xvWqyU5ykEFBFEQQMPw3HzAvWOY-_ZQAdo/preview"
+//          width="500"
+//          height="700"
+//          allow="autoplay"
+//        ></iframe>
+//      </div>
+//   );
+// }
+
+// export default App;
+
+
